564999fb0b2a9737037afbd09fe6dad1ecb68309
Dmitriy K <79517037815@yandex.ru>
2023-08-09 22:23:49 +0300

    Stats-Service started ver. 1.0

---
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -0,0 +1,54 @@
+package ru.practicum;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Component
+public class StatsClient {
+    private final RestTemplate rest;
+    private static final String STATS_SERVER_URL = "http://localhost:9090";
+
+    public StatsClient() {
+        this.rest = new RestTemplate();
+    }
+
+    public void addHit(HitDto hitDto) {
+        HttpEntity<HitDto> requestEntity = new HttpEntity<>(hitDto);
+        rest.exchange(STATS_SERVER_URL + "/hit", HttpMethod.POST, requestEntity, Object.class);
+    }
+
+    public ResponseEntity<Object> getStats(String start, String end, String[] uris, boolean unique) {
+        Map<String, Object> parameters;
+        String path;
+        if (uris != null) {
+            parameters = Map.of(
+                    "start", start,
+                    "end", end,
+                    "uris", uris,
+                    "unique", unique
+            );
+            path = STATS_SERVER_URL + "/stats/?start={start}&end={end}&uris={uris}&unique={unique}";
+        } else {
+            parameters = Map.of(
+                    "start", start,
+                    "end", end,
+                    "unique", unique
+            );
+            path = STATS_SERVER_URL + "/stats/?start={start}&end={end}&unique={unique}";
+        }
+        ResponseEntity<Object> serverResponse = rest.getForEntity(path, Object.class, parameters);
+        if (serverResponse.getStatusCode().is2xxSuccessful()) {
+            return serverResponse;
+        }
+        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(serverResponse.getStatusCode());
+        if (serverResponse.hasBody()) {
+            return responseBuilder.body(serverResponse.getBody());
+        }
+        return responseBuilder.build();
+    }
+}
