b2ec22882e7fce55e64bf73c23efce73d36737b8
Dmitriy K <79517037815@yandex.ru>
2023-08-11 17:16:35 +0300

    Diploma. Part 1. ver. 1.0

--- stats/stats-dto/src/main/java/ru/practicum/HitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/HitDto.java
@@ -5,13 +5,20 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class HitDto {
+    @NotBlank
     private String app;
+    @NotBlank
     private String uri;
+    @NotBlank
     private String ip;
+    @NotNull
     private String timestamp;
 }
