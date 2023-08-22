dbb9c955b3b60a9e5f33c523867ae0bbfd2d4ea2
Dmitriy K <79517037815@yandex.ru>
2023-08-23 02:02:45 +0300

    Second send part II.... ver 1.3

--- main-service/src/main/java/ru/practicum/event/EventRepository.java
+++ main-service/src/main/java/ru/practicum/event/EventRepository.java
@@ -2,11 +2,9 @@ package ru.practicum.event;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.persistence.LockModeType;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
