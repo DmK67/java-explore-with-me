90788c589d80d599f16b74681ae80182da6f2e50
Dmitriy K <79517037815@yandex.ru>
2023-08-29 18:41:53 +0300

    Этап 3. Дополнительная функциональность... ver. 1.1

--- main-service/src/main/java/ru/practicum/comment/CommentController.java
+++ main-service/src/main/java/ru/practicum/comment/CommentController.java
@@ -21,40 +21,40 @@ public class CommentController {
 
     @PostMapping("/user/{userId}/events/{eventId}/comment/")
     @ResponseStatus(HttpStatus.CREATED)
-    public CommentResponseDto createComment(@PathVariable @Valid @Positive Long userId,
-                                            @PathVariable @Valid @Positive Long eventId,
+    public CommentResponseDto createComment(@PathVariable @Positive Long userId,
+                                            @PathVariable @Positive Long eventId,
                                             @RequestBody @Validated NewCommentDto newCommentDto) {
         return commentService.createComment(userId, eventId, newCommentDto);
     }
 
     @GetMapping("/events/{eventId}/comments")
-    public List<CommentResponseDto> getEventComments(@PathVariable @Valid @Positive Long eventId,
+    public List<CommentResponseDto> getEventComments(@PathVariable @Positive Long eventId,
                                                      @RequestParam(defaultValue = "0") @PositiveOrZero int from,
                                                      @RequestParam(defaultValue = "10") @Positive int size) {
         return commentService.getEventComments(eventId, from, size);
     }
 
     @GetMapping("/comment/{commentId}")
-    public CommentResponseDto getCommentById(@PathVariable @Valid @Positive Long commentId) {
+    public CommentResponseDto getCommentById(@PathVariable @Positive Long commentId) {
         return commentService.getCommentById(commentId);
     }
 
     @PatchMapping("/user/{userId}/comment/{commentId}")
-    public CommentResponseDto updateComment(@PathVariable @Valid @Positive Long userId,
-                                            @PathVariable @Valid @Positive Long commentId,
+    public CommentResponseDto updateComment(@PathVariable @Positive Long userId,
+                                            @PathVariable @Positive Long commentId,
                                             @RequestBody @Validated NewCommentDto newCommentDto) {
         return commentService.updateComment(userId, commentId, newCommentDto);
     }
 
     @DeleteMapping("/user/{userId}/comment/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteComment(@PathVariable @Valid @Positive Long userId,
-                              @PathVariable @Valid @Positive Long commentId) {
+    public void deleteComment(@PathVariable @Positive Long userId,
+                              @PathVariable @Positive Long commentId) {
         commentService.deleteComment(userId, commentId);
     }
 
     @PatchMapping("/admin/comment/{commentId}")
-    public CommentResponseDto updateCommentStatusByAdmin(@PathVariable @Valid @Positive Long commentId,
+    public CommentResponseDto updateCommentStatusByAdmin(@PathVariable @Positive Long commentId,
                                                          @RequestParam boolean isConfirm) {
         return commentService.updateCommentStatusByAdmin(commentId, isConfirm);
     }
