f1cfac4fd7f84b9b6babbb432ca1cf20ee058f12
Dmitriy K <79517037815@yandex.ru>
2023-08-29 18:48:14 +0300

    Этап 3. Дополнительная функциональность... ver. 1.2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-explore-with-me
 Template repository for ExploreWithMe project.
+
+ссылка на Pull Request: 
--- main-service/src/main/java/ru/practicum/comment/CommentController.java
+++ main-service/src/main/java/ru/practicum/comment/CommentController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.comment.dto.CommentResponseDto;
 import ru.practicum.comment.dto.NewCommentDto;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
